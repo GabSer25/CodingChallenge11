5c7ac7299eb3e828723286238c65bc23659140e8
Gabriela Serrano <gabrielasc0309@gmail.com>
2025-02-26 17:56:47 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -23,3 +23,29 @@ console.log(book1.getDetails());  // Expected output: Book details
 
 book1.updateCopies(-1);  // Borrowing one copy
 console.log(book1.getDetails());  // Expected output: Copies reduced by 1
+
+// Task 2: Creating a Borrower Class
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name; // Borrower's name
+        this.borrowerId = borrowerId; // Unique borrower ID
+        this.borrowedBooks = []; // List of borrowed book titles
+    }
+
+    // Borrow a book (adds to borrowed list)
+    borrowBook(book) {
+        this.borrowedBooks.push(book);
+    }
+
+    // Return a book (removes from borrowed list)
+    returnBook(book) {
+        this.borrowedBooks = this.borrowedBooks.filter(title => title !== book);
+    }
+}
+// Test
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);  // Expected output: ["The Great Gatsby"]
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);  // Expected output: []
